c6e6e5a3aef91d28f226841c197c3c7738216355
diarcilaju <diarcilaju@uss.edu.pe>
2026-09-18 16:13:14 -0500

    Entidad Cliente

---
+++ entity/Cliente.js
@@ -0,0 +1,10 @@
+class Cliente{
+    constructor(id, nombre, apellidos, documento, telefono){
+        this.id=id;
+        this.nombre=nombre,
+        this.apellidos=apellidos;
+        this.documento=documento;
+        this.telefono=telefono;
+    }
+}
+module.exports=Cliente;
